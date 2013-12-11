d0b487df0bea68a70b59b9cd63f68e77c4e2cf11
djShrek <boho527@gmail.com>
2013-12-10 22:50:22 -0800

    commit to gh-pages

---
+++ js/parallax.js
@@ -0,0 +1,50 @@
+ $(document).ready(function(){
+
+    $(window).bind('scroll', function(e){
+        parallax();
+    });
+
+    $("a.earth").click(function(){
+        $("html, body").animate({
+            scrollTop: 0 }, 1000,
+        function(){
+            parallax();
+        });
+        return false;
+    });
+
+    $("a.moon").click(function(){
+        $("html, body").animate({
+            scrollTop: $("#moon").offset().top }, 1000,
+        function(){
+            parallax();
+        });
+        return false;
+    });
+
+    $("a.rocket").click(function(){
+        $("html, body").animate({
+            scrollTop: $("#rocket").offset().top }, 1000,
+        function(){
+            parallax();
+        });
+        return false;
+    });
+
+    $("a.saturn").click(function(){
+        $("html, body").animate({
+            scrollTop: $("#saturn").offset().top }, 1000,
+        function(){
+            parallax();
+        });
+        return false;
+    });
+
+});
+
+function parallax(){
+    var scrollPosition = $(window).scrollTop();
+    console.log(scrollPosition);
+    $("#stars").css("top", (0 - (scrollPosition * 0.2)) + 'px');
+    $("#images").css("top", (0 - (scrollPosition * 0.5)) + 'px');
+}
